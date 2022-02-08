6aff853934a411a0638ceb6ee6503d2f0dde9b0e
mmeow0 <tumerkinaa@gmail.com>
2022-02-08 16:38:22 +0300

    добавлено удаление/отображение записей

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,11 @@
-
-from elasticsearch import Elasticsearch
 from flask import Flask
-from config.config import Config
+from elasticsearch import Elasticsearch
+from app.config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
     if app.config['ELASTICSEARCH_URL'] else None
-
 from app import views
 
--- app/views.py
+++ app/views.py
@@ -1,12 +1,45 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
-from config.config import Config
+
 
 
 @app.route('/')
 def home():
-   return str(Config.ELASTICSEARCH_URL)
+    return render_template("home.html")
 
-@app.route('/template')
-def template():
-    return render_template('home.html')
+
+@app.route("/search", methods=['POST'])
+def search_es():
+    if request.method == 'POST':
+        query = request.form['search']
+    data = app.elasticsearch.search(
+        index="posts",
+        body={"query": {
+            "multi_match": {
+                "query": query,
+                "fields": ["text"]}},
+            'size': 20}
+    )
+    posts = [{"id": data["_id"],
+              "text": data["_source"]["text"],
+              "rubrics": data["_source"]["rubrics"],
+              "created_date": data["_source"]["created_date"]}
+             for data in data['hits']['hits']]
+    posts.sort(key=lambda item:item['created_date'])
+    if not posts:
+        info="По вашему запросу ничего не найдено"
+
+    return render_template("index.html", data=posts, info=info)
+
+
+@app.route("/delete", methods=['POST'])
+def delete_es():
+    if request.method == 'POST':
+        id = request.form['delete']
+    res = app.elasticsearch.delete(index="posts", doc_type='_doc', id=id)
+    if "result" in res and res["result"] == "deleted":
+        res="Post deleted"
+    else:
+        res = "Error while deleting"
+    return render_template("delete.html", data=res)
+
